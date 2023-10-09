746a6697e7427cc24dea29591a05073219fba877
WeepingHeron <kimtaehoon_98@naver.com>
2023-10-09 13:02:50 +0900

    ChangeToRateDiscountPolicy

--- Roadmap_01/core/src/main/java/hello/core/AppConfig.java
+++ Roadmap_01/core/src/main/java/hello/core/AppConfig.java
@@ -2,6 +2,7 @@ package hello.core;
 
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
@@ -23,6 +24,7 @@ public class AppConfig {
     }
 
     public DiscountPolicy discountPolicy() {
-        return new FixDiscountPolicy();
+        // return new FixDiscountPolicy();
+        return new RateDiscountPolicy();
     }
 }
