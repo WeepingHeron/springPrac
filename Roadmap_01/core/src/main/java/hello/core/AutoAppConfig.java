9c54214bd4a9714679c396e204826f08a93e13f2
WeepingHeron <kimtaehoon_98@naver.com>
2023-10-11 13:48:14 +0900

    VariousDI

--- Roadmap_01/core/src/main/java/hello/core/AutoAppConfig.java
+++ Roadmap_01/core/src/main/java/hello/core/AutoAppConfig.java
@@ -1,16 +1,25 @@
 package hello.core;
 
+import hello.core.discount.DiscountPolicy;
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+import hello.core.order.OrderService;
+import hello.core.order.OrderServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
 
 @Configuration
 @ComponentScan(
-        basePackages = "hello.core.member",
-        basePackageClasses = AutoAppConfig.class,
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
 
 public class AutoAppConfig {
 
+    @Bean(name = "memoryMemberRepository")
+    MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
 }
--- Roadmap_01/core/src/test/java/hello/core/order/OrderServiceTest.java
+++ Roadmap_01/core/src/test/java/hello/core/order/OrderServiceTest.java
@@ -1,9 +1,11 @@
 package hello.core.order;
 
 import hello.core.AppConfig;
+import hello.core.discount.FixDiscountPolicy;
 import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
+import hello.core.member.MemoryMemberRepository;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
