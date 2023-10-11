085cc7d357e360c1fdf5d8453077df0250278940
WeepingHeron <kimtaehoon_98@naver.com>
2023-10-11 14:02:37 +0900

    Optional

--- Roadmap_01/core/src/main/java/hello/core/AutoAppConfig.java
+++ Roadmap_01/core/src/main/java/hello/core/AutoAppConfig.java
@@ -18,8 +18,8 @@ import org.springframework.context.annotation.FilterType;
 
 public class AutoAppConfig {
 
-    @Bean(name = "memoryMemberRepository")
-    MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
-    }
+//    @Bean(name = "memoryMemberRepository")
+//    MemberRepository memberRepository() {
+//        return new MemoryMemberRepository();
+//    }
 }
