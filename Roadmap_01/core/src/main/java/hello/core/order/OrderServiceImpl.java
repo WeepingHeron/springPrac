8bd8237832a41d7e0b07ef8f39794507a559c329
WeepingHeron <kimtaehoon_98@naver.com>
2023-10-11 17:03:18 +0900

    NoUniqueBeanDefinitionException

--- Roadmap_01/core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ Roadmap_01/core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -4,14 +4,20 @@ import hello.core.discount.DiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
-@RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService {
 
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
+
+    @Autowired
+    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
