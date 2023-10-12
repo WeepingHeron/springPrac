40d44df3987ba4fc4b7da6d50c6f48e5cf31068a
WeepingHeron <kimtaehoon_98@naver.com>
2023-10-12 19:45:26 +0900

    InitializingBeanAndDisposableBean

--- Roadmap_01/core/src/test/java/hello/core/lifecycle/NetworkClient.java
+++ Roadmap_01/core/src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -1,6 +1,9 @@
 package hello.core.lifecycle;
 
-public class NetworkClient {
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
+public class NetworkClient implements InitializingBean, DisposableBean {
     
     private String url;
     
@@ -25,6 +28,19 @@ public class NetworkClient {
     
     // 서비스 종료 시 호출
     public void disconnect() {
-        System.out.println("close" + url);
+        System.out.println("close : " + url);
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("NetworkClient.afterPropertiesSet");
+        connect();
+        call("초기화 연결 메시지");
+    }
+
+    @Override
+    public void destroy() throws Exception {
+        System.out.println("NetworkClient.destroy");
+        disconnect();
     }
 }
