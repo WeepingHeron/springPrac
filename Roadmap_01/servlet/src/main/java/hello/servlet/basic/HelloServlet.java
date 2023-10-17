d9b1da596361d8dd5bd8e1d07c20199ba1bcb7f2
WeepingHeron <kimtaehoon_98@naver.com>
2023-10-17 14:12:53 +0900

    HelloServlet

--- Roadmap_01/servlet/src/main/java/hello/servlet/basic/HelloServlet.java
+++ Roadmap_01/servlet/src/main/java/hello/servlet/basic/HelloServlet.java
@@ -11,8 +11,9 @@ import java.io.IOException;
 public class HelloServlet extends HttpServlet {
 
     @Override
-    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+    protected void service(HttpServletRequest request, HttpServletResponse
+            response)
+            throws ServletException, IOException {
         System.out.println("HelloServlet.service");
         System.out.println("request = " + request);
         System.out.println("response = " + response);
