1f65eb3a938bd7d351a2fed7e7cc5a5433a9c3da
WeepingHeron <kimtaehoon_98@naver.com>
2023-10-27 17:33:12 +0900

    ViewResolver

--- Roadmap_01/servlet/src/main/java/hello/servlet/ServletApplication.java
+++ Roadmap_01/servlet/src/main/java/hello/servlet/ServletApplication.java
@@ -3,6 +3,8 @@ package hello.servlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @ServletComponentScan // 서블릿 자동 등록
 @SpringBootApplication
@@ -10,6 +12,11 @@ public class ServletApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ServletApplication.class, args);
+	}
+
+	@Bean
+	InternalResourceViewResolver internalResourceViewResolver() {
+		return new InternalResourceViewResolver("/WEB-INF/views/", ".jsp");
 	}
 
 }
