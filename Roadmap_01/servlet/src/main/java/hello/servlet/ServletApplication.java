8870639b5ed5cc13182f2cf26e4fd9212e25c8ca
WeepingHeron <kimtaehoon_98@naver.com>
2023-10-30 11:57:51 +0900

    ControllerV1

--- Roadmap_01/servlet/src/main/java/hello/servlet/ServletApplication.java
+++ Roadmap_01/servlet/src/main/java/hello/servlet/ServletApplication.java
@@ -3,8 +3,6 @@ package hello.servlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @ServletComponentScan // 서블릿 자동 등록
 @SpringBootApplication
@@ -14,9 +12,9 @@ public class ServletApplication {
 		SpringApplication.run(ServletApplication.class, args);
 	}
 
-	@Bean
-	InternalResourceViewResolver internalResourceViewResolver() {
-		return new InternalResourceViewResolver("/WEB-INF/views/", ".jsp");
-	}
+//	@Bean
+//	SpringMemberFormControllerV1 springMemberFormControllerV1() {
+//		return new SpringMemberFormControllerV1();
+//	}
 
 }
